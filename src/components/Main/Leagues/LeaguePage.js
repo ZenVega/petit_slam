478771f978a679d17dfc79e3b13d121ecc4ca1f6
ZenVega <ufogreeks@gmail.com>
2021-04-17 19:09:59 +0200

    issue with removing player from league

--- src/components/Main/Leagues/LeaguePage.js
+++ src/components/Main/Leagues/LeaguePage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
+import { useFirebase } from 'react-redux-firebase'
 
 
 import { getLeagueById, getPlayersInLeague, getAdminsInLeague } from '../../../selectors/index'
@@ -13,6 +14,7 @@ import PlayerCard from '../Players/PlayerCard'
 function LeaguePage() {
   
   let { leagueId } = useParams();
+  const firebase = useFirebase();
   const league = useSelector(getLeagueById(leagueId))
   const players = useSelector(getPlayersInLeague(leagueId))
   const admins = useSelector(getAdminsInLeague(leagueId))
@@ -20,6 +22,23 @@ function LeaguePage() {
   const ownId = useSelector(state => state.firebase.auth.uid)
   const inLeague = players && players.indexOf(ownId) != -1? true: false
   const isAdmin = admins && admins.indexOf(ownId) != -1? true: false
+
+  const leaveLeague = () => {
+    firebase.remove(`leagues/${leagueId}/players/`, ownId)
+    .then( cred => {console.log('removed from league',cred)})
+    .catch(err => console.log(err))
+    
+    if(isAdmin){
+      firebase.remove(`leagues/${leagueId}/admins/`, ownId)
+    .then( cred => {console.log('removed from leagues admins',cred)})
+    .catch(err => console.log(err))
+    }
+   
+    //this deletes all the players leagues
+    firebase.remove(`users/${ownId}/leagues/`, leagueId)
+    .then( cred => {console.log('league removed from users data',cred)})
+    .catch(err => console.log(err))
+  }
 
   if (!isLoaded(players)) {
     return <div>Loading...</div>
@@ -40,7 +59,10 @@ function LeaguePage() {
       <h1>{league.leagueType}</h1>
       <h2>status: {league.status}</h2>
       {isAdmin && <h2>You are an admin of this league</h2> }
-      {!inLeague? <button>join league</button>: <button>leave league</button>}
+      {!inLeague
+      ? <button>join league</button>
+      : <button
+        onClick={leaveLeague}>leave league</button>}
       <h2>players</h2>
       {players.map((player, index) => (
         <PlayerCard
